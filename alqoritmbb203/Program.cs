a69ba10563fdcadcee88e87ab4713f650a4fbcc2
gunaybabayeva21 <gunaybabayeva032@gmail.com>
2023-03-28 11:04:24 +0400

    first commit

---
+++ alqoritmbb203/Program.cs
@@ -0,0 +1,47 @@
+﻿//task1
+
+//int num1 = 16;
+//int num2 = 42;
+//int sum = 0;
+//for (int i = num1; i <= num2; i++)
+//{
+//    if (i % 7 == 0 & i % 3 == 0)
+//    {
+//        sum+=i;
+//        Console.WriteLine(sum);
+//    }
+//}
+
+
+//task2
+
+using System.ComponentModel.Design;
+
+//int num =456;
+
+//if (num%2==0)
+//{
+
+//    Console.WriteLine("murekkeb ededdir");
+//}
+//else 
+//{
+//    Console.WriteLine("sade ededdir");
+//}
+
+
+
+
+
+//task3
+
+//int num = 456;
+//if(num/100==4)
+//{
+//    Console.WriteLine("3 mertebelidir");
+
+//}
+//else
+//{
+//    Console.WriteLine(" 3 mertebeli deyil");
+//}
